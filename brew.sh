212016d12ef4552ef77b6c3f96c639c17d9c18dd
wuxianjie <wuxianjiezh@gmail.com>
2021-01-12 18:52:08 +0800

    remove some outdated casks

--- brew.sh
+++ brew.sh
@@ -14,10 +14,4 @@ brew install node
 brew install yarn
 
 # Install MacOS Applications
-brew install --cask intellij-idea
-brew install --cask webstorm
-brew install --cask postman
-brew install --cask google-chrome
-brew install --cask firefox
-brew install --cask macvim
 brew install --cask odlp/bluesnooze/bluesnooze
